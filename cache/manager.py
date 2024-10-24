ebfe76604f47d716812e43c614d9297dbfae3ab8
Takkeshi <tacomastabusiness@gmail.com>
2024-10-24 11:53:41 -0600

    Refactor CacheManager setup to include error handling and update database schema

--- cache/manager.py
+++ cache/manager.py
@@ -6,31 +6,50 @@ logger = logging.getLogger(__name__)
 
 class CacheManager:
     def __init__(self): 
-        self.__setup__()
+        if not self.__setup__():
+            raise Exception("Failed to setup cache")
     
     def __setup__(self):
         try:
             self.conn = sqlite3.connect(f"{pathlib.Path(__file__).parent}\\cache.db")
             self.cursor = self.conn.cursor()
             
+            logger.info("__SETUP__ : Setting up github schema")
             self.cursor.execute("""
-                CREATE TABLE IF NOT EXISTS github_visited (
+                CREATE TABLE IF NOT EXISTS github_config (
                     id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    key TEXT NOT NULL,
+                    value TEXT NOT NULL
+                )
+            """)
+            self.cursor.execute("""
+                CREATE TABLE IF NOT EXISTS github_keywords (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    keyword TEXT NOT NULL,
+                    score INTEGER NOT NULL,
+                    rules TEXT NULL
+                )
+            """)
+            self.cursor.execute("""
+                CREATE TABLE IF NOT EXISTS github_whitelisted (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    name TEXT NOT NULL,
                     url TEXT NOT NULL,
-                    date TEXT NOT NULL
+                    score INTEGER NOT NULL
                 )
             """)
-            logger.info("__SETUP__ : ")
             self.cursor.execute("""
-                CREATE TABLE IF NOT EXISTS github_tracking (
+                CREATE TABLE IF NOT EXISTS github_blacklisted (
                     id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    username TEXT NOT NULL,
-                    plataform TEXT NOT NULL,
-                    date TEXT NOT NULL
+                    name TEXT NOT NULL,
+                    reason TEXT NOT NULL
                 )
             """)
+            
+            return True
         except Exception as e:
             logger.error(f"__SETUP__ : {e}")
+            return False
     
     def using_squema(self, schema: str):
         return self.SchemaEmulator(schema)
